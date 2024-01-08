ee47336aba366828d6ccee1db305ecbf84552752
IamShihabPro <mshihab.pro@gmail.com>
2024-01-08 23:31:36 +0600

    delete from cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleware
@@ -67,6 +67,12 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
+    })
 
 
     await client.db("admin").command({ ping: 1 });
