157a43bf912464520f2887011e560a34e8e775f5
IamShihabPro <mshihab.pro@gmail.com>
2024-01-11 02:42:38 +0600

    all users

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function run() {
 
 
     // users collection
+
+    app.get('/users', async (req, res) =>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
     app.post('/users', async(req, res)=>{
       const user = req.body
       const query = {email: user.email}
