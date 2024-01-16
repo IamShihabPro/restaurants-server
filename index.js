12974d66ef00c3ae22bbfd954eee494e69bd40b7
IamShihabPro <mshihab.pro@gmail.com>
2024-01-16 23:05:19 +0600

    admin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const verifyJWT = (req, res, next) =>{
   // bearer token
   const token = authorization.split(' ')[1]
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded) =>{
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
     if(err){
       return res.status(401).send({error: true, message: 'Unauthorize access'})
     }
@@ -75,6 +75,18 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/admin/:email', verifyJWT, async(req, res)=>{
+      const email = req.params.email
+      if(req.decoded.email !== email){
+        return res.send({admin: false})
+      }
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin : user?.role === 'admin'}
+      res.send(result)
+    })
+
+
     app.patch('/users/admin/:id', async(req, res) =>{
       const id = req.params.id
       const filter = {_id: new ObjectId(id)}
@@ -104,19 +116,18 @@ async function run() {
     // cart collection
     app.get('/carts', verifyJWT, async(req, res)=>{
       const email = req.query.email
+      // console.log(email);
       if(!email){
         res.send([])
       }
-
-      const decodedEmail = req.decoded.email
-      if(email !== decodedEmail){
-        return res.status(403).send({error: true, message: 'Forbidden access'})
-      }
-
-      const query = {email: email}
-      const result = await cartCollection.find(query).toArray()
-      res.send(result) 
-    })
+     const decodedEmail = req.decoded.email
+     if(email !== decodedEmail){
+      return res.status(403).send({error: true, message: 'forbidden access'}) 
+     }
+     const query = { email: email}
+     const result = await cartCollection.find(query).toArray()
+     res.send(result)
+  })
 
     app.post('/carts', async(req, res) =>{
       const item = req.body
