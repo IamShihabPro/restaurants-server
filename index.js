f3cccbd8ea3eddaca2c03423033c57d65de080e8
IamShihabPro <mshihab.pro@gmail.com>
2024-01-23 20:03:36 +0600

    update menu

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ async function run() {
       res.send(result);
     });
 
-    app.put('/menu/:id', async(req, res)=>{
+    app.put('/menu/:id', verifyJWT, verifyAdmin, async(req, res)=>{
       const id = req.params.id
       const filter = {_id: new ObjectId(id)}
       const options = {upsert: true}
